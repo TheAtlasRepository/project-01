43374b06fa25b39a4bbf38f4060195bbad0fb5a3
Trivinyx <tom.a.s.myre@gmail.com>
2024-04-03 12:54:49 +0200

    removing test creation endpoint
    -related files removed

--- backend/img2mapAPI/__init__.py
+++ backend/img2mapAPI/__init__.py
@@ -1,6 +1,5 @@
 from . import Img2mapAPI
 from . import routers
 from . import utils
-from . import devOnly
 
-__all__ = ["Img2mapAPI", "routers", "utils", "devOnly"]
+__all__ = ["Img2mapAPI", "routers", "utils"]
