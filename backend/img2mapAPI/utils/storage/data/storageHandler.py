dd368597f6de303c6e55673172da7de2e3478c0c
Trivinyx <tom.a.s.myre@gmail.com>
2024-04-18 09:19:53 +0200

    Docstring creation and updates 3
    - Removed some unused imports
    - Updated the docstring for the functions and classes (created when non exsistant)

--- backend/img2mapAPI/utils/storage/data/storageHandler.py
+++ backend/img2mapAPI/utils/storage/data/storageHandler.py
@@ -1,93 +1,114 @@
+""" This module contains the abstract base class for moving data to and from the storage/Database
+"""
+
 from abc import ABC, abstractmethod
 from typing import List, Union
 from pydantic import BaseModel
 
-#Abstract class for moving data to and from storage/DB and the API server
 class StorageHandler(ABC):
-    #abstarct fileds
+    """This class is the abstract base class for moving data to and from the storage/Database.
+
+    The class is intended to be inherited by classes that implement the storage/DB connection and overide data manipulation functions in this class.
+    Intended use as a singleton class. The class is a singleton class.
+    
+    Abstarct functions:
+        connect: Connect to the storage
+        saveInStorage: Save data to storage
+        remove: Remove data from storage
+        update: Update data in storage
+        fetchOne: Fetch one data from storage
+        fetch: Fetch data from storage
+        fetchAll: Fetch all data from storage
+    """
+
     _instance = None
 
     #abstract methods
     @abstractmethod
     async def connect(self, db: str, user: str, password: str, host: str, port: int)->any:
-        """
-        Connect to the storage
-        
-        Arguments:
-            db {str} -- The name of the database
-            user {str} -- The username
-            password {str} -- The password
-            host {str} -- The host
-            port {int} -- The port
+        """Connect to the storage
 
-        return {any} -- The connection to the storage/DB or None if the connection failed
+        Args:
+            db (str): database name
+            user (str): Db user profile name
+            password (str): Db user password
+            host (str): Db host
+            port (int): Db port
+
+        Returns:
+            any: The connection object
         """
         pass
 
     @abstractmethod
     async def saveInStorage(self, data: BaseModel, type: str, pkName:str = 'id')->int:
-        """
-        Save data to storage and return the id of the saved data
-        
-        Arguments:
-            data {any} -- The data to be saved
-            type {str} -- The type of the data / the table name / the model class name
-            pkName {str} -- The primary key name
-        return {int} -- The id of the saved data
+        """Save data to storage
+
+        Args:
+            data (BaseModel): The data to be saved
+            type (str): The type of the data / the table name / the model class name
+            pkName (str, optional): the fieldname for the Primary Key in database/storage. Defaults to 'id'.
+
+        Returns:
+            int: The id of the saved data from the storage
         """
         pass
 
     @abstractmethod
     async def remove(self, id: int, type: str)->None:
-        """
-        Remove data from storage
+        """Remove data from storage
 
-        Arguments:
-            id {int} -- The id of the data
-            type {str} -- The type of the data / the table name / the model class name
+        Args:
+            id (int): The id of the data
+            type (str): The type of the data / the table name / the model class name
         """
         pass
 
     @abstractmethod
     async def update(self, id: int, data: BaseModel, type: str)->None:
-        """
-        Update data in storage
-        
-        Arguments:
-            id {int} -- The id of the data
-            data {any} -- The data to be updated
-            type {str} -- The type of the data / the table name / the model class name
+        """Update data in storage
+
+        Args:
+            id (int): The id of the data
+            data (BaseModel): The data to be updated
+            type (str): The type of the data / the table name / the model class name
         """
         pass
     
     @abstractmethod
     async def fetchOne(self, id: int, type: str) -> Union[None, dict]:
-        """
-        Fetch one data from storage
+        """Fetch one "Row" of data from storage
 
-        Arguments:
-            id {int} -- The id of the data
-            type {str} -- The type of the data / the table name / the model class name
+        Args:
+            id (int): The id of the data
+            type (str): The type of the data / the table name / the model class name
+
+        Returns:
+            Union[None, dict]: The data fetched from the storage. None if no data is found or an error occured.
         """
         pass
 
     @abstractmethod
     async def fetch(self, type: str, params: dict = {})->Union[None ,List[dict], dict]:
-        """
-        Fetch data from storage
-        
-        Arguments:
-            type {str} -- The type of the data / the table name / the model class name
-            params {dict} -- The parameters to filter the data
+        """Fetch data from storage
+
+        Args:
+            type (str): The type of the data / the table name / the model class name
+            params (dict, optional): dict of additional fields [Key] to search and the value to match by [value] . Defaults to {}.
+
+        Returns:
+            Union[None ,List[dict], dict]: _description_
         """
         pass
 
     @abstractmethod
     async def fetchAll(self, type: str)->Union[None, List[dict]]:
-        """
-        Fetch all data from storage
+        """Fetch all data from storage
 
-        Arguments:
-            type {str} -- The type of the data / the table name / the model class name
+        Args:
+            type (str): The type of the data / the table name / the model class name
+
+        Returns:
+            Union[None, List[dict]]: The data fetched from the storage. None if no data is found or an error occured.
         """
         pass
--- backend/img2mapAPI/utils/storage/files/localFileStorage.py
+++ backend/img2mapAPI/utils/storage/files/localFileStorage.py
@@ -3,65 +3,75 @@ import shutil
 import tempfile
 from .fileStorage import FileStorage
 from ...core.FileHelper import clearTmpFolder
-from fastapi import UploadFile
 
 _tempPath = "./temp"
 if not os.path.exists(_tempPath):
     os.makedirs(_tempPath)
 
-# Clear files in the temp folder
 clearTmpFolder()
 
-#File storage using the local file system
+
 class LocalFileStorage(FileStorage):
-    #temp folder should be in main script folder
+    """ This class is the file storage implementation for local storage.
+
+    The class implements the file manipulation functions in the abstract base class FileStorage
+
+    Functions:
+        saveFile: Save a file to local storage
+        removeFile: Remove a file from local storage
+        readFile: Read a file from local storage
+        fileExists: Check if a file exists in local storage
+        saveFileFromPath: Save a file to local storage from a path
+        getInstance: Get the instance of the class
+
+    Attributes:
+        tempPath: The path to the temp folder
+        tempFolder: The path to the temp folder 
+    """
+
     _instance = None
     tempPath = _tempPath
     tempFolder = tempfile.mkdtemp(dir=_tempPath)
    
     async def saveFile(self, data: tempfile , suffix: str) -> str:
-        #save the file
-        #create a temp file that will be deleted when the function ends
         with tempfile.NamedTemporaryFile(dir=self.tempFolder, suffix=suffix, delete=False) as file:
             file.write(data)
             path = file.name
         return path
+    
     async def removeFile(self, path: str):
-        #remove the file
         os.remove(path)
+    
     async def readFile(self, path: str)->bytes:
-        #open the file
         with open(path, "rb") as file:
             data = file.read()
         return data
+    
     async def fileExists(self, path: str)->bool:
-        #check if the file exists
         return os.path.isfile(path)
     
     async def saveFileFromPath(self, path: str, suffix: str)->str:
-        #copy the file to the temp folder
         with open(path, "rb") as file:
             data = file.read()
         return await self.saveFile(data, suffix)
 
     @staticmethod
     def getInstance():
+        """Get the instance of the class
+        """
+        
         if LocalFileStorage._instance is None:
             LocalFileStorage._instance = LocalFileStorage()
         return LocalFileStorage._instance
     
     def __del__(self):
-        #remove the temp folder
         try:
             os.rmdir(self.tempFolder)
         except:
-            #use shutil to remove the folder if it is not empty
             shutil.rmtree(self.tempFolder)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        #remove the temp folder
         os.rmdir(self.tempFolder)
-        #remove the temp folder if it is empty
         if len(os.listdir(self.tempPath)) == 0:
             os.rmdir(self.tempPath)
   
