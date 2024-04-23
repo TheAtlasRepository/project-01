c13f92ca817579d6b6f7cc8f77b8a63706a05bc8
Trivinyx <tom.a.s.myre@gmail.com>
2024-04-23 12:31:20 +0200

    Initial untested handler logic
    All abstract functions implemented and following sql stm have been added.
    
    Not tested in current because of no implementation of getting the database connection_url

--- backend/img2mapAPI/utils/core/helper/postgresSqlHelper.py
+++ backend/img2mapAPI/utils/core/helper/postgresSqlHelper.py
@@ -5,33 +5,42 @@
 
 import psycopg2
 import os
-from psycopg2 import sql
-from psycopg2.extras import DictCursor
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.errors import DuplicateTable
 
 
-def createDB(dbname: str, user: str, password: str, host: str, port: str):
+def createDB(database_url: str, datname: str = 'img2map'):
+    """Create a database if it does not exist
+
+    Args:
+        database_url (str): Connection string to the database
+        datname (str, optional): Name for the database. Defaults to 'img2map'.
+    """
     #check if the database exists, if not create it
-    conn = psycopg2.connect(dbname='postgres', user=user, password=password, host=host, port=port)
+    conn = psycopg2.connect(database_url, sslmode='require')
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
-    cur.execute(sql.SQL("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s"), [dbname])
+    cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (datname,))
     exists = cur.fetchone()
     if not exists:
-        cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(dbname)))
+        cur.execute("CREATE DATABASE %s", (datname,))
     cur.close()
     conn.close()
 
-def createTable(dbname: str, user: str, password: str, host: str, port: str, table_name: str, columns: dict):
-    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+def createTable(datbase_url: str, sql: str):
+    """Create a table in the database
+
+    Args:
+        datbase_url (str): Connection string to the database
+        sql (str): The sql query to create the table
+    """
+    conn = psycopg2.connect(datbase_url, sslmode='require')
     cur = conn.cursor()
-    columns_str = ', '.join([f"{k} {v}" for k, v in columns.items()])
     try:
-        cur.execute(sql.SQL(f"CREATE TABLE {table_name} ({columns_str})"))
+        cur.execute(sql)
     except DuplicateTable:
         pass
-    conn.commit()
     cur.close()
     conn.close()
+
 
--- backend/img2mapAPI/utils/storage/data/postgresSqlHandler.py
+++ backend/img2mapAPI/utils/storage/data/postgresSqlHandler.py
@@ -1,4 +1,5 @@
 """ """
+from typing import List, Union
 import psycopg2
 from pydantic import BaseModel
 from img2mapAPI.utils.storage.data.storageHandler import StorageHandler as sh
@@ -12,8 +13,10 @@ class PostgresSqlHandler(sh):
     """
 
     _instance = None
-
+    
+    doneSetup = False
     conn = None
+    database_url = None
 
     async def connect(self, db: str, user: str, password: str, host: str, port: int)->any:
         """Connect to the storage
@@ -28,7 +31,10 @@ class PostgresSqlHandler(sh):
         Returns:
             any: The connection object
         """
-        conn = psycopg2.connect(dbname=db, user=user, password=password, host=host, port=port)
+        if self.database_url is not None:
+            conn = psycopg2.connect(self.database_url)
+        else:
+            conn = psycopg2.connect(dbname=db, user=user, password=password, host=host, port=port)
         return conn
 
     async def saveInStorage(self, data: BaseModel, type: str, pkName:str = 'id')->int:
@@ -42,7 +48,74 @@ class PostgresSqlHandler(sh):
         Returns:
             int: The id of the saved data from the storage
         """
-        return 0
+        
+        if (self.setupDone == False): await self.setup() #make sure the database is setup
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+        returnid: int = 0
+        if type == 'project':
+            sql = f"INSERT INTO project
+                (
+                    name,
+                    description,
+                    crs,
+                    imageFilePath,
+                    georeferencedFilePath,
+                    selfdestructtime,
+                    created, 
+                    lastModified
+                ) VALUES (
+                    '{data.name}',
+                    '{data.description}', 
+                    '{data.crs}', 
+                    '{data.imageFilePath}', 
+                    '{data.georeferencedFilePath}', 
+                    '{data.selfdestructtime}', 
+                    '{data.created}', 
+                    '{data.lastModified}')"
+            try:
+                cur.execute(sql)
+                conn.commit()
+                returnid = cur.lastrowid
+            except Exception as e:
+                print(e)
+                return 0
+            finally:
+                cur.close()
+                conn.close()
+        if type == 'point':
+            sql = f"INSERT INTO point
+                (
+                    projectId,
+                    Idproj,
+                    lat,
+                    lng,
+                    row,
+                    col,
+                    error,
+                    name,
+                    description
+                ) VALUES (
+                    '{data.projectId}',
+                    '{data.Idproj}',
+                    '{data.lat}',
+                    '{data.lng}',
+                    '{data.row}',
+                    '{data.col}',
+                    '{data.error}',
+                    '{data.name}',
+                    '{data.description}')"
+            try:
+                cur.execute(sql)
+                conn.commit()
+                returnid = cur.lastrowid
+            except Exception as e:
+                print(e)
+                return 0
+            finally:
+                cur.close()
+                conn.close()
+        return returnid
 
     async def remove(self, id: int, type: str)->None:
         """Remove data from storage
@@ -51,7 +124,21 @@ class PostgresSqlHandler(sh):
             id (int): The id of the data
             type (str): The type of the data / the table name / the model class name
         """
-        pass
+
+        if (self.setupDone == False): await self.setup() #make sure the database is setup
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+        try:
+            sql = f"DELETE FROM {type.lower} WHERE id = {id}"
+            cur.execute(sql)
+            conn.commit()
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            cur.close()
+            conn.close()
+            print(f"Deleted {type} with id {id}") #Todo: log this
 
     async def update(self, data: BaseModel, type: str, pkName:str = 'id')->None:
         """Update data in storage
@@ -61,9 +148,52 @@ class PostgresSqlHandler(sh):
             type (str): The type of the data / the table name / the model class name
             pkName (str, optional): the fieldname for the Primary Key in database/storage. Defaults to 'id'.
         """
-        pass
 
-    async def fetchOne(self, id: int, type: str)->BaseModel:
+        if (self.setupDone == False): await self.setup() #make sure the database is setup
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+        if type == 'project':
+            sql = f"UPDATE project SET
+                name = '{data.name}',
+                description = '{data.description}',
+                crs = '{data.crs}',
+                imageFilePath = '{data.imageFilePath}',
+                georeferencedFilePath = '{data.georeferencedFilePath}',
+                selfdestructtime = '{data.selfdestructtime}',
+                lastModified = '{data.lastModified}'
+                WHERE id = {data.id}"
+            try:
+                cur.execute(sql)
+                conn.commit()
+            except Exception as e:
+                print(e)
+                pass
+            finally:
+                cur.close()
+                conn.close()
+        if type == 'point':
+            sql = f"UPDATE point SET
+                projectId = '{data.projectId}',
+                Idproj = '{data.Idproj}',
+                lat = '{data.lat}',
+                lng = '{data.lng}',
+                row = '{data.row}',
+                col = '{data.col}',
+                error = '{data.error}',
+                name = '{data.name}',
+                description = '{data.description}'
+                WHERE id = {data.id}"
+            try:
+                cur.execute(sql)
+                conn.commit()
+            except Exception as e:
+                print(e)
+                pass
+            finally:
+                cur.close()
+                conn.close()
+
+    async def fetchOne(self, id: int, type: str)->Union[None, dict]:
         """Fetch one data from storage
 
         Args:
@@ -73,8 +203,83 @@ class PostgresSqlHandler(sh):
         Returns:
             BaseModel: The fetched data
         """
-        return BaseModel
+
+        if (self.setupDone == False): await self.setup() #make sure the database is setup
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+        try:
+            sql = f"SELECT * FROM {type} WHERE id = {id}"
+            cur.execute(sql)
+            data = cur.fetchone()
+            return data._asdict()
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            cur.close()
+            conn.close()
     
+    async def fetch(self, type: str, params: dict = {})->Union[None ,list, dict]:
+        """Fetch data from storage
+
+        Args:
+            type (str): The type of the data / the table name / the model class name
+            params (dict, optional): dict of additional fields [Key] to search and the value to match by [value] . Defaults to {}.
+
+        Returns:
+            list: The fetched data
+        """
+
+        if (self.setupDone == False): await self.setup()
+
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+        try:
+            sql = f"SELECT * FROM {type}"
+            if len(params) > 0:
+                sql += " WHERE "
+                for key, value in params.items():
+                    sql += f"{key} = {value} AND "
+                sql = sql[:-4] #remove the last AND
+            cur.execute(sql)
+            data = cur.fetchall()
+            return data
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            cur.close()
+            conn.close()
+        return None
+    
+    async def fetchAll(self, type: str) ->Union[None, list]:
+        """Fetch all data from storage
+
+        Args:
+            type (str): The type of the data / the table name / the model class name
+
+        Returns:
+            None | List[dict]: The fetched data
+        """
+
+        if (self.setupDone == False): await self.setup()
+        conn = psycopg2.connect(self.database_url)
+        cur = conn.cursor()
+
+        try:
+            sql = f"SELECT * FROM {type}"
+            cur.execute(sql)
+            data = cur.fetchall()
+            return data
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            cur.close()
+            conn.close()
+        return None
+    
+
     @staticmethod
     def getInstance():
         """Get the instance of the class
@@ -86,22 +291,70 @@ class PostgresSqlHandler(sh):
             PostgresSqlHandler._instance = PostgresSqlHandler()
         return PostgresSqlHandler._instance
     
-    def __init__(self, database_url: str = None):
+
+
+    async def createModelTable(self):
+        """Create a tables for the project model & point model part of the database setup
+
+        """
+        #Crating table if not exists for project model
+        project_table = """CREATE TABLE IF NOT EXISTS project(
+                    id  int PRIMARY KEY AUTOINCREMENT,
+                    name VARCHAR (80) NOT NULL,
+                    description TEXT,
+                    crs VARCHAR (25),
+                    imageFilePath VARCHAR (255),
+                    georeferencedFilePath VARCHAR (255),
+                    selfdestructtime TIMESTAMPTZ,
+                    created TIMESTAMPTZ,
+                    lastModified TIMESTAMPTZ
+                );
+            """
+        try:
+            await createTable(self.database_url, project_table)
+        except Exception as e:
+            print(e)
+            pass #Todo: handle exception
+
+        points_table = """CREATE TABLE IF NOT EXISTS point(
+                    id INTEGER PRIMARY KEY,
+                    projectId INTEGER,
+                    Idproj INTEGER NOT NULL,
+                    lat REAL NOT NULL,
+                    lng REAL NOT NULL,
+                    row INTEGER NOT NULL,
+                    col INTEGER NOT NULL,
+                    error REAL,
+                    name VARCHAR (80),
+                    description TEXT,
+                    CONSTRAINT fk_project
+                        FOREIGN KEY(projectId)
+                            REFERENCES project(id) ON DELETE SET NULL
+                );
+            """
+        try:
+            await createTable(self.database_url, points_table)
+        except Exception as e:
+            print(e)
+            pass #Todo: handle exception
+
+    async def setup(self):
+        """Setup the database
+
+        """
+        try:
+            await createDB(self.database_url)
+            await self.createModelTable()
+            self.doneSetup = True
+        except Exception as e:
+            print(e)
+            pass
+    
+    def __init__(self, database_url: str):
         """Constructor for the class
         """
-        #first check if  the connection is healthy
-        try:
-            self.conn = self.connect(
-                db=DATABASE,
-                user=USER,
-                password=PASSWORD,
-                host=HOST,
-                port=PORT
-            )
-        except Exception as e:
+        self.database_url = database_url
 
-        pass
-    
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(PostgresSqlHandler, cls).__new__(cls)
