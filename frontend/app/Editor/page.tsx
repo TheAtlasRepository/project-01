9f551e245341fa4cb74406d86c5588f6855d300c
mnilsen <32636970+ostepizza@users.noreply.github.com>
2024-04-24 12:45:20 +0200

    Changed Editor component to be dynamic

--- frontend/app/Editor/page.tsx
+++ frontend/app/Editor/page.tsx
@@ -1,16 +1,15 @@
 'use client';
-import Editor from '../../components/component/editor';
-import { useRouter } from 'next/navigation';
+import dynamic from 'next/dynamic';
+
+const DynamicEditor = dynamic(
+  () => import('../../components/component/editor'),
+  { ssr: false }
+);
 
 export default function Page() {
-    const router = useRouter();
-    // check window object to see if we are in the browser or not then redirect to the home page
-    if (typeof window == 'undefined') {
-        router.push('/');
-    }
-    return (
-        <main>
-            {typeof window !== 'undefined' ? <Editor /> : null} 
-        </main>
-    );
+  return (
+    <main>
+      <DynamicEditor />
+    </main>
+  );
 }
