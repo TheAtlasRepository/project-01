d72ebbc6aa11771e4f2fa45fc50bd151979c54cb
Meepception <35727117+Meepception@users.noreply.github.com>
2024-05-02 14:31:16 +0200

    Some Helptext
    Some helptext so far, still needs more work.

--- frontend/components/ui/HelpModal.tsx
+++ frontend/components/ui/HelpModal.tsx
@@ -12,10 +12,50 @@ const HelpModal: React.FC<HelpModalProps> = ({ onClose, currentPage, setCurrentP
         <div>
             <h1 className='text-3xl font-bold'>Welcome!</h1>
             <hr className='my-3' />
-            <p>Welcome to the georeferencing tool! Let's start with a quick recap; what is georeferencing?</p>
+            <p>Welcome to the georeferencing tool! Let's start with a quick recap; what is georeferencing? <br></br>
+            Georeferencing is the process of assigning real-world coordinates to each pixel of a digital image or map.By doing this, 
+            the image or map can be accurately placed on a global map in its real world location. This allows the image to be viewed and analyzed in relation to other geographical data.
+
+            </p>
         </div>,
-        'Page 2 content',
-        'Page 3 content',
+        <div>
+        <h1 className='text-3xl font-bold'>Welcome!</h1>
+        <hr className='my-3' />
+        <p>Understanding how to navigate the interface will help you make the most of our nifty features. <br></br>
+        <b>Split View:</b> Initially, you will see a split-screen layout with a globe-map on the left and your uploaded image or pdf page on the right. 
+        You will be able to move your uploaded file around the canvas as well as zoom in and out. You can move around the globe by using the naviagtion buttons located bottom right,
+        or by dragging and zooming. Alternatively you can also use the search box in the bottom left
+        to find the location you want. The globe map also has a button on the top left for toggling between Streets/Satelite view. <br></br>
+        <b>Navbar:</b> The navbar at the top of the screen contains various useful buttons. You can toggle between Split View and Overlay (Overlay is only available once you've georeferenced your image), 
+        Crop which will open your uploaded file in a window where you can crop it, Help will open this guide, Feedback opens a form you can fill out if you have any feedback you'd like to give us, Download
+        will download your georeferenced file as a .TIFF file, and finally Exit Editor will take you back to the main page. <br></br>
+        <b>Overlay:</b> When you have georeferenced your file you can switch to the Overlay mode where the image is superimposed directly over the map. 
+        This mode is helpful for comparing your alignment and to ensure the accuracy of your georeferencing. Adjust the image overlay opacity to see how the image fits over the map terrain using the slider on the top left.
+        </p>
+    </div>,
+    
+        <div>
+        <h1 className='text-3xl font-bold'>Welcome!</h1>
+        <hr className='my-3' />
+        <b>Adding Points in order to georeference:</b>
+        Accurate georeferencing depends significantly on correctly placing enough points on both the map and the corresponding image. Here's how you can effectively add points to ensure precise alignment.
+        <br></br>
+        <b>Starting the Process:</b>
+        <ul>
+
+            <li>● Begin by identifying clear landmarks on your image that are also visible on the map. These could be building corners, intersections, or distinctive natural features.</li>
+            <li>● Click anywhere on the map or your image, the crosshair tool will appear. Move the crosshair to where you want to add the point and confirm. Now click on the opposite view (map or image) 
+                of the one you placed a point on, move the crosshair to the corresponding location and confirm. Repeat this process until you have 3 points or more. </li>
+            <li>● If you need to delete a point you can manage your points by clicking on the button labeled "Coordinates". 
+                There your points will be listed with their coordinates on the globe map and the pixel coordinates on your image.</li>
+        </ul>
+
+
+      
+        
+
+        
+    </div>,,
         // Add more pages as needed
     ];
 
