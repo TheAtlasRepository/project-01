30e1d90d1e7979917621b7a751e64350bbe5627e
sebmid <sebmid@live.com>
2024-05-02 12:10:13 +0200

    Added tooltops to buttons on hover

--- frontend/components/ui/ZoomButtons.tsx
+++ frontend/components/ui/ZoomButtons.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { ResetIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface ZoomButtonsProps {
   setZoomLevel: React.Dispatch<React.SetStateAction<number>>;
@@ -22,31 +28,45 @@ export default function ZoomButtons({ setZoomLevel }: ZoomButtonsProps) {
   };
 
   return (
-    <div
-      className="absolute bottom-2 right-0 m-4 gap-2 flex flex-col z-10 text-black dark:text-white
-    "
-    >
-      <div className="flex flex-col gap-1">
-        <Button onClick={zoomIn} variant={"zoom"} size={"zoom"}>
-          +
-        </Button>
-        <Button
-          onClick={zoomOut}
-          variant={"zoom"}
-          size={"zoom"}
-          //   className="w-8 h-8 bg-white rounded-md hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"
-        >
-          -
-        </Button>
+    <TooltipProvider>
+      <div className="absolute bottom-2 right-0 m-4 gap-2 flex flex-col z-10 text-black dark:text-white">
+        <div className="flex flex-col gap-1">
+          <Tooltip>
+            <TooltipTrigger>
+              <Button onClick={zoomIn} variant={"zoom"} size={"zoom"}>
+                +
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Zoom In</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button onClick={zoomOut} variant={"zoom"} size={"zoom"}>
+                -
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Zoom Out</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button
+              onClick={() => setZoomLevel(1)}
+              variant={"zoom"}
+              size={"zoom"}
+            >
+              <ResetIcon />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Reset Zoom</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
-      <Button
-        onClick={() => setZoomLevel(1)}
-        variant={"zoom"}
-        size={"zoom"}
-        // className="w-8 h-8 bg-white rounded-md hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"
-      >
-        <ResetIcon />
-      </Button>
-    </div>
+    </TooltipProvider>
   );
 }
